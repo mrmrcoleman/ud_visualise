e6ec2660bc52c2c48795aaea1ff0bbcdb296c33b
Adrian Mouat <adrian.mouat@gmail.com>
2014-02-13 12:56:24 +0100

    Loading image & static starting image.

--- visualise/visualise/app/views.py
+++ visualise/visualise/app/views.py
@@ -5,10 +5,13 @@ from app import app
 @app.route('/visualise', methods=['GET', 'POST'])
 def visualise():
     index = "RDSB"
+    dynamic = False
     if request.method == 'POST':
         index = request.form['index']
+        dynamic = True
     return render_template('visualise.html',
-                           index=index)
+                           index=index,
+                           dynamic=dynamic)
 
 
 @app.route('/')
