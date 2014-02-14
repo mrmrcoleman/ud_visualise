be5d092a8ec735c5a7cdba51ca0dbba88d1da8c8
mrmrcoleman <m.coleman@uglyduckling.nl>
2014-02-14 12:09:44 +0000

    Removed the push from the build step and added the build step to the wercker.yml

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 cd pythonjs
 sudo docker build -rm -t mrmrcoleman/pythonjs .
-sudo docker push mrmrcoleman/pythonjs
+#sudo docker push mrmrcoleman/pythonjs
 cd ../visualise
 sudo docker build -rm -t mrmrcoleman/visualise .
