7e63fa18f5d44adab21367b5ba6a591137262025
Adrian Mouat <adrian.mouat@gmail.com>
2014-02-14 16:59:38 +0100

    Minor changes to text and CSS.

--- visualise/visualise/app/views.py
+++ visualise/visualise/app/views.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, request
 from app import app
 
 
-@app.route('/visualise', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def visualise():
     index = "RDSB"
     dynamic = False
@@ -12,9 +12,3 @@ def visualise():
     return render_template('visualise.html',
                            index=index,
                            dynamic=dynamic)
-
-
-@app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    return redirect(url_for('visualise'))
