87c752a4e9b482b2ced3f247b1791bfea2c88aa0
Adrian Mouat <adrian.mouat@gmail.com>
2014-02-20 12:18:07 +0100

    Split out responsibilities.
     - Remove references to visualise from base docker file
     - Rename pythonjs to python_webapp

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -vxe
 
 cd pythonjs
-sudo docker build -rm -t mrmrcoleman/pythonjs .
+sudo docker build -rm -t mrmrcoleman/python_webapp .
 #sudo docker push mrmrcoleman/pythonjs
 cd ../visualise
 sudo docker build -rm -t mrmrcoleman/visualise .
